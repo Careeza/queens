98110cb92cce5b8f7c53ad21c5fd8d99909bcc0f
VinCheetah <vincent.gardies@ens-lyon.fr>
2024-11-21 17:51:55 +0100

    Update

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame as pg
+import matplotlib.pyplot as plt
 
 
 class Board:
@@ -9,12 +10,22 @@ class Board:
         self.board_matrix = board_matrix
         self.screen = self.game.screen
         self.board_width, self.board_height = self.board_matrix.shape
-        self.n_queen = np.unique(self.board_matrix)
-        self.colors = [None for i in range(self.n_queen)]
+        self.n_queen = len(np.unique(self.board_matrix))
+        cmap = plt.cm.get_cmap('tab10', self.n_queen) 
+        self.colors_list = [tuple(int(c * 255) for c in cmap(i)[:3]) for i in range(self.n_queen)]
+        self.colors = [[] for _ in range(self.n_queen)]
         self.player_matrix = np.zeros((self.board_width, self.board_height), dtype=int)
         self.cells = np.empty((self.board_width, self.board_height), dtype=pg.Rect)
+        self.subcells = np.empty((self.board_width, self.board_height), dtype=pg.Rect)
         self.queens_placed = 0
+        
+        self.allow_double_colors = True
+        self.allow_wrong_placements = True
+        self.queen_display_mode = "geometric"
+        
+        self.subcell_ratio = 0.8
         self.create_cells()
+        
         
     @property
     def cell_width(self):
@@ -29,28 +40,32 @@ class Board:
         self.display()
         
     def create_cells(self):
+        sub_width = self.cell_width * self.subcell_ratio
+        sub_height = self.cell_height * self.subcell_ratio
         for i in range(self.board_width):
             for j in range(self.board_height):
-                self.cases[i, j] = pg.Rect(i * self.cell_width, j * self.cell_height, self.cell_width, self.cell_height)
+                self.cells[i, j] = pg.Rect(i * self.cell_width, j * self.cell_height, self.cell_width, self.cell_height)
+                self.subcells[i, j] = pg.Rect(i * self.cell_width + (self.cell_width - sub_width) / 2, j * self.cell_height + (self.cell_height - sub_height) / 2, sub_width, sub_height)
                 
     def create_grid(self):
         for i in range(self.board_width):
             pg.draw.line(self.screen, (0, 0, 0), (i * self.cell_width, 0), (i * self.cell_width, self.game.height))
         
         for j in range(self.board_height):
-            pg.draw.line(self.screen, (0, 0, 0), (0, i * self.cell_height), (self.game.width, i * self.cell_height))
+            pg.draw.line(self.screen, (0, 0, 0), (0, j * self.cell_height), (self.game.width, j * self.cell_height))
         
         
     def display(self):
-        self.create_grid()
         
         for i in range(self.board_width):
             for j in range(self.board_height):
                 pg.draw.rect(self.screen, self.get_col(self.board_matrix[i, j]), self.cells[i, j])
+        
+        self.create_grid()
                 
                 
     def get_col(self, value):
-        return 0, 255 * value // self.n_queen, 255 - (255 * value) // self.n_queen
+        return self.colors_list[value]
         
     
     def can_place_queen(self, i, j):
@@ -61,9 +76,9 @@ class Board:
         for _i in range(self.board_width):
             if self.player_matrix[_i, j] == 1 and self.board_matrix[_i, j] != col:
                 return False
-            
+        
         adjacents = [(i + dep_i, j + dep_j) for dep_i in [-1, 0, 1]
-                                            for dep_j in [-1, 0, 1] 
+                                            for dep_j in [-1, 0, 1]
                                             if (dep_i != 0 or dep_j != 0) 
                                                 and 0 <= i + dep_i < self.board_width 
                                                 and 0 <= j + dep_j < self.board_height
@@ -71,48 +86,112 @@ class Board:
         for adj in adjacents:
             if self.player_matrix[adj] == 1:
                 return False
+        return True
     
         
     def get_cell(self, pos):
         return pos[0] // self.cell_width, pos[1] // self.cell_height
+    
+    def victory(self, debug=False):
+        if self.n_queen != self.queens_placed:
+            if debug:
+                print(f"Wrong number of queens: {self.n_queen} not equal to {self.queens_placed}")
+            return False
+        if not all(map(len, self.colors)):
+            if debug:
+                print(f"No queen for all colors: ", all(map(len, self.colors)), map(len, self.colors))
+            return False
+        for i in range(self.board_width):
+            for j in range(self.board_height):
+                if self.board_matrix[i, j] == 1:
+                    print(i, j)
+                    break
+            else:
+                if debug:
+                    print(f"No queen in line {i}")
+                return False
+        for j in range(self.board_height):
+            for i in range(self.board_width):
+                if self.board_matrix[i, j] == 1:
+                    print(i, j)
+                    break
+            else:
+                if debug:
+                    print(f"No queen in column {j}")
+                return False
+        return True
+    
+    def check_victory(self):
+        if self.victory():
+            self.game.victory()
     
     def left_click(self, pos):
         i, j = self.get_cell(pos)
         col = self.board_matrix[i, j]
         
         if self.player_matrix[i, j] == 1:
-            print("Queen removed")
-            self.player_matrix[i, j] = 0
-            self.colors[col] = None
-            self.queens_placed -= 1
+            self.remove_queen(i, j)
             return
         
         if self.player_matrix[i, j] == 2:
             print("No Queen here")
             return
         
-        if not self.can_place_queen(i, j):
+        if not self.allow_wrong_placements and not self.can_place_queen(i, j):
             print("Cannot place Queen here")
             return
 
+        if self.colors[col] != [] and not self.allow_double_colors:
+            for i, j in self.colors[col]:
+                self.remove_queen(i, j)
+        self.place_queen(i, j)
+    
+    def remove_queen(self, i, j):
+        print("Queen removed")
+        self.player_matrix[i, j] = 0
+        self.colors[self.board_matrix[i, j]].remove((i, j))
+        self.queens_placed -= 1
+        
+        pg.draw.rect(self.screen, self.get_col(self.board_matrix[i, j]), self.subcells[i, j])
+        self.check_victory()
+        
+    def remove_noqueen(self, i, j):
+        self.player_matrix[i, j] = 0
+        
+        pg.draw.rect(self.screen, self.get_col(self.board_matrix[i, j]), self.subcells[i, j])
+        
+        
+    def place_queen(self, i, j):
         print("Queen placed")
         self.player_matrix[i, j] = 1
-        if self.colors[col] is not None:
-            self.queens_placed -= 1
-        self.colors[col] = i, j
+        self.colors[self.board_matrix[i, j]].append((i, j))
         self.queens_placed += 1
         
+        if self.queen_display_mode == "geometric":
+            pg.draw.rect(self.screen, (0, 0, 0), self.subcells[i, j])
+        self.check_victory()
+        
+    def place_noqueen(self, i, j):
+        self.player_matrix[i, j] = 2
+        
+        if self.queen_display_mode == "geometric":
+            pg.draw.rect(self.screen, (255, 255, 255), self.subcells[i, j])
         
     def right_click(self, pos):
         i, j = self.get_cell(pos)
         
         if self.player_matrix[i, j] == 2:
-            print("No Queen already here")
+            self.remove_noqueen(i, j)
             return
         
         if self.player_matrix[i, j] == 1:
             print("Queen already here")
             return
         
-        print("No Queen placed")
-        self.player_matrix[i, j] = 2
+        self.place_noqueen(i, j)
+        
+    def reset_player(self):
+        self.player_matrix = np.zeros((self.board_width, self.board_height), dtype=int)
+        self.queens_placed = 0
+        self.colors = [[] for _ in range(self.n_queen)]
+        self.init_display()
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from board import Board
 import numpy as np
 import pygame as pg
+import random as rd
 
 
 class Game:
@@ -21,24 +22,54 @@ class Game:
         
         while self.running:
             self.interact()
-            self.board.init_display()
             pg.display.flip()
         
         
     def new_board(self):
         self.create_board_matrix()
         self.board = Board(self, self.board_matrix)
+        self.board.init_display()
+        
+        
+    def victory(self):
+        self.running = False
+        print("Victory !")
         
     def create_board_matrix(self):
-        raise NotImplementedError
+        self.board_matrix = rd.choice([np.array([[0,0,0,0,1,1,1,1],
+                                [0,0,1,1,1,2,2,3],
+                                [0,0,5,0,2,2,2,3],
+                                [4,5,5,0,3,3,3,3],
+                                [4,5,5,0,6,0,0,0],
+                                [5,5,5,6,6,6,0,0],
+                                [5,5,6,6,6,7,0,7],
+                                [5,5,6,0,6,7,7,7],
+                                ]), 
+                                np.array([[0,0,1,1,1,0,2,0],
+                                [0,1,1,1,2,2,2,0],
+                                [1,1,3,4,2,2,0,0],
+                                [5,5,3,4,4,2,2,0],
+                                [5,5,4,4,4,2,2,0],
+                                [5,5,4,4,4,2,2,6],
+                                [5,5,7,4,7,6,0,6],
+                                [5,5,7,7,7,6,6,6],
+                                ])])
         
     def interact(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                pg.quit()
                 self.running = False
-            if event.type == pg.MOUSEBUTTONDOWN:
+            elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.board.left_click(event.pos)
                 elif event.button == 3:
                     self.board.right_click(event.pos)
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.running = False
+                if event.key == pg.K_r:
+                    self.board.reset_player()
+                elif event.key == pg.K_n:
+                    self.new_board()
+                elif event.key == pg.K_w:
+                    print(self.board.victory(debug=True))
